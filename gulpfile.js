e5c511d5a486635cac2c7312b292e8236b0fa053
kawanamiyuu <i.am.really.you@gmail.com>
2015-12-06 18:08:48 +0900

    HTMLHintによるHTMLの静的解析

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,45 @@
 'use strict';
 
 var gulp = require('gulp');
+var htmlhint = require('gulp-htmlhint');
 var eslint = require('gulp-eslint');
 var csslint = require('gulp-csslint');
+
+gulp.task('htmlhint', function() {
+	return gulp.src('index.html')
+		.pipe(htmlhint({
+			// https://github.com/yaniswang/HTMLHint/wiki/Rules
+			// Standard
+			'tagname-lowercase': true,
+			'attr-lowercase': true,
+			'attr-value-double-quotes': true,
+			'attr-value-not-empty': true,
+			'attr-no-duplication': true,
+			'doctype-first': true,
+			'tag-pair': true,
+			'tag-self-close': true,
+			'spec-char-escape': true,
+			'id-unique': true,
+			'src-not-empty': true,
+			'title-require': true,
+			// Performance
+			'head-script-disabled': false,
+			// Accessibility
+			'alt-require': false,
+			// Specification
+			'doctype-html5': true,
+			'id-class-value': 'dash',
+			'style-disabled': true,
+			'inline-style-disabled': true,
+			'inline-script-disabled': true,
+			'space-tab-mixed-disabled': 'tab',
+			'id-class-ad-disabled': true,
+			'href-abs-or-rel': false,
+			'attr-unsafe-chars': true
+		}))
+		.pipe(htmlhint.reporter())
+		.pipe(htmlhint.reporter('fail'));
+});
 
 gulp.task('csslint', function () {
 	return gulp.src(['css/**/*.css', '!css/lib/**'])
@@ -24,4 +61,4 @@ gulp.task('eslint', function () {
 		.pipe(eslint.failOnError());
 });
 
-gulp.task('lint', ['csslint', 'eslint']);
+gulp.task('lint', ['htmlhint', 'csslint', 'eslint']);
