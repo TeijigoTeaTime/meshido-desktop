63c761946047e0d28d18a3d761d9fd87385f66a0
kawanamiyuu <i.am.really.you@gmail.com>
2015-12-10 20:09:05 +0900

    アプリ起動時、ユーザ情報がストレージになければ、ログインダイアログを表示する。ログイン処理後、ユーザ情報をストレージに保存する #4

--- js/index.js
+++ js/index.js
@@ -1,44 +1,24 @@
 'use strict';
 
 $(document).ready(function() {
-	console.log('index.js');
-});
 
-$(document).ready(function() {
-	var $loginModal = $('#login-modal');
-
-	// TODO： ログインしていないときだけ
-	// ログインダイアログを開く
-	$loginModal.openModal({
-		dismissible: false,
-		opacity: .2,
-		in_duration: 300
+	localforage.config({
+		driver: localforage.LOCALSTORAGE
 	});
 
-	$loginModal.on('click', '.js-login-btn', function(e) {
-		e.preventDefault();
+	localforage.getItem('user').then(function(user) {
+		console.log(user);
 
-		var email = $('#login-email').val();
-		var name = $('#login-name').val();
-
-		login(email, name).done(function(sessionId) {
-			// TODO: セッションIDを永続化する
-			console.log(sessionId);
-			$loginModal.closeModal();
-		});
+		if (user) {
+			// ログイン済みの場合
+			// カレンダーを表示
+			$(document).trigger('display-calendar');
+		} else {
+			// ログインダイアログを表示
+			$(document).trigger('open-login-modal');
+		}
+	}, function(err) {
+		console.log(err);
 	});
 
-	/**
-	 * サーバにログイン情報を送信し、セッションIDを受け取る
-	 *
-	 * @param {String} email メールアドレス
-	 * @param {String} name 名前
-	 * @returns {Promise}
-	 */
-	var login = function(email, name) {
-		// TODO: サーバにリクエストを投げる
-		var defer = $.Deferred();
-		defer.resolve('session-id');
-		return defer.promise();
-	};
 });
---
+++ js/login.js
@@ -0,0 +1,61 @@
+'use strict';
+
+$(document).ready(function() {
+
+	var $loginModal = $('#login-modal');
+
+	/**
+	 * ログインダイアログを表示するEvent
+	 */
+	$(document).on('open-login-modal', function() {
+		$loginModal.openModal({
+			dismissible: false,
+			opacity: .2,
+			in_duration: 300
+		});
+	});
+
+	/**
+	 * ログインボタン押下時のEvent
+	 */
+	$loginModal.on('click', '.js-login-btn', function(e) {
+		e.preventDefault();
+
+		var email = $('#login-email').val();
+		var name = $('#login-name').val();
+
+		login(email, name).then(function(user) {
+			// ストレージにユーザ情報を保存
+			localforage.setItem('user', user).then(function() {
+				console.log(user);
+
+				// ログインダイアログを閉じる
+				$loginModal.closeModal();
+				// カレンダーを表示する
+				$(document).trigger('display-calendar');
+
+			}, function(err) {
+				console.log(err);
+			});
+		});
+	});
+
+	/**
+	 * サーバにログイン情報を送信し、セッションIDを受け取る
+	 *
+	 * @param {String} email メールアドレス
+	 * @param {String} name 名前
+	 * @returns {Promise}
+	 */
+	var login = function(email, name) {
+		// TODO: サーバにリクエストを投げる
+		var defer = $.Deferred();
+		defer.resolve({
+			email: email,
+			name: name,
+			token: 'api-token'
+		});
+		return defer.promise();
+	};
+
+});
