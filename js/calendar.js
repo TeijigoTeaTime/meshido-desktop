a2bbe7e3eb426f7b8b0b292c23350d545a76791c
masato-n <rakus.masato.nishihara@gmail.com>
2015-12-15 01:01:25 +0900

    #6　コードレビュー後の修正2

--- js/calendar.js
+++ js/calendar.js
@@ -5,31 +5,29 @@ $(document).ready(function () {
 	 * カレンダーを表示するイベント
 	 */
 	$(document).on('display-calendar', function () {
-		$(function() {
-			function updateMonthYear() {
-				$( '#custom-month' ).html( $( '#calendar' ).calendario('getMonthName') );
-				$( '#custom-year' ).html( $( '#calendar' ).calendario('getYear'));
-			}
-			$(document).on('finish.calendar.calendario', function(e){
-				$( '#custom-month' ).html( $( '#calendar' ).calendario('getMonthName') );
-				$( '#custom-year' ).html( $( '#calendar' ).calendario('getYear'));
-				$( '#custom-next' ).on( 'click', function() {
-					$( '#calendar' ).calendario('gotoNextMonth', updateMonthYear);
-				} );
-				$( '#custom-prev' ).on( 'click', function() {
-					$( '#calendar' ).calendario('gotoPreviousMonth', updateMonthYear);
-				} );
-				$( '#custom-current' ).on( 'click', function() {
-					$( '#calendar' ).calendario('gotoNow', updateMonthYear);
-				} );
-			});
-			$( '#calendar' ).calendario({
-				checkUpdate : false,
-				caldata : events,
-				fillEmpty : false
-			});
+		function updateMonthYear() {
+			$( '#custom-month' ).html( $( '#calendar' ).calendario('getMonthName') );
+			$( '#custom-year' ).html( $( '#calendar' ).calendario('getYear'));
+		}
+		$(document).on('finish.calendar.calendario', function(e){
+			$( '#custom-month' ).html( $( '#calendar' ).calendario('getMonthName') );
+			$( '#custom-year' ).html( $( '#calendar' ).calendario('getYear'));
+			$( '#custom-next' ).on( 'click', function() {
+				$( '#calendar' ).calendario('gotoNextMonth', updateMonthYear);
+			} );
+			$( '#custom-prev' ).on( 'click', function() {
+				$( '#calendar' ).calendario('gotoPreviousMonth', updateMonthYear);
+			} );
+			$( '#custom-current' ).on( 'click', function() {
+				$( '#calendar' ).calendario('gotoNow', updateMonthYear);
+			} );
 		});
-		console.log('display-calendar');
+		$( '#calendar' ).calendario({
+			checkUpdate : false,
+				caldata : events,
+			fillEmpty : false
+		});
 	});
+	console.log('display-calendar');
 });
 
