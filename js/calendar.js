a3a9f831118beac04e6de79fcf19b655713e8aa6
masato-n <rakus.masato.nishihara@gmail.com>
2015-12-19 18:41:39 +0900

    #8　レビュー後修正

--- js/calendar.js
+++ js/calendar.js
@@ -7,28 +7,27 @@ $(document).ready(function () {
 	 * カレンダーを表示するイベント
 	 */
 	$(document).on('display-calendar', function () {
-		function updateMonthYear() {
-			$( '#custom-month' ).html( $( '#calendar' ).calendario('getMonthName') );
-			$( '#custom-year' ).html( $( '#calendar' ).calendario('getYear'));
-		}
-		$(document).on('finish.calendar.calendario', function(e){
-			$( '#custom-month' ).html( $( '#calendar' ).calendario('getMonthName') );
-			$( '#custom-year' ).html( $( '#calendar' ).calendario('getYear'));
-			$( '#custom-next' ).on( 'click', function() {
-				$( '#calendar' ).calendario('gotoNextMonth', updateMonthYear);
-			} );
-			$( '#custom-prev' ).on( 'click', function() {
-				$( '#calendar' ).calendario('gotoPreviousMonth', updateMonthYear);
-			} );
-			$( '#custom-current' ).on( 'click', function() {
-				$( '#calendar' ).calendario('gotoNow', updateMonthYear);
-			} );
-		});
 		$( '#calendar' ).calendario({
 			checkUpdate : false,
 				caldata : buildCalData(fetchCalendar(2015, 12)),
 			fillEmpty : false
 		});
+	});
+
+	$(document).on('finish.calendar.calendario', function () {
+		updateYearMonth();
+	});
+
+	$('#custom-next').on('click', function () {
+		$calendar.calendario('gotoNextMonth', updateYearMonth);
+	});
+
+	$('#custom-prev').on('click', function () {
+		$calendar.calendario('gotoPreviousMonth', updateYearMonth);
+	});
+
+	$('#custom-current').on('click', function () {
+		$calendar.calendario('gotoNow', updateYearMonth);
 	});
 
 	/**
